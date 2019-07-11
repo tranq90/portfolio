cb73584544aa18a0e22e144c5b4d5c7f80ded98c
Quan Tran <tran.q90@gmail.com>
2019-07-11 01:01:22 -0700

    added different sections

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Content from "./components/Content/Content";
+import Navbar from "./components/Navbar/Navbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Portfolio">
+        <React.Fragment>
+          <Navbar/>
+          <Content/>
+        </React.Fragment>
       </div>
     );
   }
---
+++ src/components/Content/Content.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Content extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="welcome">
+
+                </div>
+                <div className="about">
+
+                </div>
+                <div className="projects">
+
+                </div>
+                <div className="contact">
+
+                </div>
+                <div className="footer">
+
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Content;
